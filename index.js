f7930469f60dd9e77595b0e708ff2b350b02187a
ctyper824 <ctyper824@gmail.com>
2025-03-05 08:34:08 -0800

    came back to origin :back:

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 // import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import booksRoute from "../routes/booksRoute.js";
+import booksRoute from "./routes/booksRoute.js";
 import dotenv from "dotenv";
 import cors from "cors";
 
