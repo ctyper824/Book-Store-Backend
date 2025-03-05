800b672709c4829ac91580f66592e51e32a0f2b8
ctyper824 <ctyper824@gmail.com>
2025-03-05 06:48:29 -0800

    updated cors origin :hammer:

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // React app runs on this port
+    origin: "http://localhost:3000/", // React app runs on this port
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
